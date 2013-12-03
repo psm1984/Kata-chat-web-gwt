c27d6317763138b571d2b1acc134c8eb9479e502
psm1984 <psm1984@gmail.com>
2013-12-03 12:57:52 +0100

    Versión inicial del proyecto

---
+++ src/main/java/ChatKata/client/ChatKataModule.java
@@ -0,0 +1,164 @@
+package ChatKata.client;
+
+import ChatKata.shared.FieldVerifier;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
+import com.google.gwt.event.dom.client.KeyUpEvent;
+import com.google.gwt.event.dom.client.KeyUpHandler;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.*;
+
+import sun.security.util.Password;
+
+public class ChatKataModule implements EntryPoint {
+
+  private static final String SERVER_ERROR = "An error occurred while "
+      + "attempting to contact the server. Please check your network "
+      + "connection and try again.";
+
+
+  private final GreetingServiceAsync greetingService = GWT.create(GreetingService.class);
+  private final Messages messages = GWT.create(Messages.class);
+
+
+  public void onModuleLoad() {
+    final Button sendButton = new Button( messages.sendButton() );
+    final TextBox nameField = new TextBox();
+    final PasswordTextBox passwordTextBox = new PasswordTextBox();
+
+
+    final Label nameFieldCheck     = new Label();
+
+    final Label passwordFieldCheck = new Label();
+
+
+    nameField.setText( messages.nameField() );
+    final Label errorLabel = new Label();
+
+
+    sendButton.addStyleName("sendButton");
+
+
+    RootPanel.get("nameFieldContainer").add(nameField);
+    RootPanel.get("passwordFieldContainer").add(passwordTextBox);
+
+    RootPanel.get("nameFieldCheck").add(nameFieldCheck);
+    RootPanel.get("passwordFieldCheck").add(passwordFieldCheck);
+
+    RootPanel.get("sendButtonContainer").add(sendButton);
+    RootPanel.get("errorLabelContainer").add(errorLabel);
+
+
+
+    nameFieldCheck.setText("x");
+    passwordFieldCheck.setText("x");
+
+    nameField.setFocus(true);
+    nameField.selectAll();
+
+    // Create the popup dialog box
+    final DialogBox dialogBox = new DialogBox();
+    dialogBox.setText("Remote Procedure Call");
+    dialogBox.setAnimationEnabled(true);
+    final Button closeButton = new Button("Close");
+    // We can set the id of a widget by accessing its Element
+    closeButton.getElement().setId("closeButton");
+    final Label textToServerLabel = new Label();
+    final HTML serverResponseLabel = new HTML();
+
+    VerticalPanel dialogVPanel = new VerticalPanel();
+    dialogVPanel.addStyleName("dialogVPanel");
+    dialogVPanel.add(new HTML("<b>Sending name to the server:</b>"));
+    dialogVPanel.add(textToServerLabel);
+    dialogVPanel.add(new HTML("<br><b>Server replies:</b>"));
+    dialogVPanel.add(serverResponseLabel);
+    dialogVPanel.setHorizontalAlignment(VerticalPanel.ALIGN_RIGHT);
+    dialogVPanel.add(closeButton);
+    dialogBox.setWidget(dialogVPanel);
+
+    // Add a handler to close the DialogBox
+    closeButton.addClickHandler(new ClickHandler() {
+      public void onClick(ClickEvent event) {
+        dialogBox.hide();
+        sendButton.setEnabled(true);
+        sendButton.setFocus(true);
+      }
+    });
+
+    // Create a handler for the sendButton and nameField
+    class MyHandler implements ClickHandler, KeyUpHandler {
+      /**
+       * Fired when the user clicks on the sendButton.
+       */
+      public void onClick(ClickEvent event) {
+        sendNameToServer();
+      }
+
+      /**
+       * Fired when the user types in the nameField.
+       */
+      public void onKeyUp(KeyUpEvent event) {
+          //final TextBox nameField = new TextBox();
+          //final PasswordTextBox passwordTextBox = new PasswordTextBox();
+        Object source = event.getSource();
+        if (source.equals(nameField)) {
+
+          if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+            sendNameToServer();
+          } else {
+
+          }
+        } else if(source.equals(passwordTextBox)) {
+
+
+        }
+      }
+
+      /**
+       * Send the name from the nameField to the server and wait for a response.
+       */
+      private void sendNameToServer() {
+        // First, we validate the input.
+        errorLabel.setText("");
+        String textToServer = nameField.getText();
+        textToServer += " " + passwordTextBox.getText();
+        if (!FieldVerifier.isValidName(textToServer)) {
+          errorLabel.setText("Please enter at least four characters");
+          return;
+        }
+
+        // Then, we send the input to the server.
+        sendButton.setEnabled(false);
+        textToServerLabel.setText(textToServer);
+        serverResponseLabel.setText("");
+        greetingService.greetServer(textToServer, new AsyncCallback<String>() {
+          public void onFailure(Throwable caught) {
+            // Show the RPC error message to the user
+            dialogBox.setText("Remote Procedure Call - Failure");
+            serverResponseLabel.addStyleName("serverResponseLabelError");
+            serverResponseLabel.setHTML(SERVER_ERROR);
+            dialogBox.center();
+            closeButton.setFocus(true);
+          }
+
+          public void onSuccess(String result) {
+            dialogBox.setText("Remote Procedure Call");
+            serverResponseLabel.removeStyleName("serverResponseLabelError");
+            serverResponseLabel.setHTML(result);
+            dialogBox.center();
+            closeButton.setFocus(true);
+          }
+        });
+      }
+    }
+
+    // Add a handler to send the name to the server
+    MyHandler handler = new MyHandler();
+    sendButton.addClickHandler(handler);
+    nameField.addKeyUpHandler(handler);
+    passwordTextBox.addKeyUpHandler(handler);
+  }
+}
